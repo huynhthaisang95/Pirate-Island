6abb01830d9a2c88c2cf41ba96431b9bc5fa7f89
phoong <thanhphoongnguyen1@gmail.com>
2020-06-15 19:50:17 +0700

    commit island 2

---
+++ Assets/Scripts/Controller/MainmenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainmenuController : MonoBehaviour {
+
+    public void _PlayIsland1() {
+
+        SceneManager.LoadScene("Island1");
+        Time.timeScale = 1f;
+    }
+
+    public void _PlayIsland2()
+    {
+        SceneManager.LoadScene("Island2");
+        Time.timeScale = 1f;
+    }
+
+    public void _PlayIsland3()
+    {
+        SceneManager.LoadScene("Island3");
+        Time.timeScale = 1f;
+    }
+    public void _Quit() {
+        Application.Quit();
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
